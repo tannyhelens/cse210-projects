431b5656f7ac459cc8137505357362e9afd53387
tannyhelens <tsantos7@byupathway.edu>
2026-08-12 21:37:22 -0600

    Complete W07 Exercise Tracking Program

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,35 @@
+public abstract class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public DateTime GetDate()
+    {
+        return _date;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    public abstract double GetDistance();
+
+    public abstract double GetSpeed();
+
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} {GetType().Name} ({_minutes} min) - " +
+               $"Distance: {GetDistance():0.0} miles, " +
+               $"Speed: {GetSpeed():0.0} mph, " +
+               $"Pace: {GetPace():0.0} min per mile";
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,34 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        Running running = new Running(
+            new DateTime(2026, 8, 12),
+            30,
+            3.0
+        );
+
+        Cycling cycling = new Cycling(
+            new DateTime(2026, 8, 12),
+            45,
+            12.0
+        );
+
+        Swimming swimming = new Swimming(
+            new DateTime(2026, 8, 12),
+            30,
+            20
+        );
+
+        activities.Add(running);
+        activities.Add(cycling);
+        activities.Add(swimming);
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
